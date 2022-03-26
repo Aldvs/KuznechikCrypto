0e482dd25db4ff42366d5d4c51375ad31cc35321
aldvs <andrewgogol777@gmail.com>
2022-03-26 22:00:25 +0300

    1. The values for the nonlinear S transformation and reverse S transformation are determined. (pi and reversePi arrays) 2. Determined values of L-vector for R transformation. 3. Initialized arrays for iterative Constants and Keys.

--- test.playground/Contents.swift
+++ test.playground/Contents.swift
@@ -1,24 +1,27 @@
 import Cocoa
 
-var fullKey = "8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef"
-
-//func getPartOfFullKey(key fullKey: String) -> [String] {
+//var fullKey = "8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef"
 //
-var pairOfKyes = [String]()
+////func getPartOfFullKey(key fullKey: String) -> [String] {
+////
+//var pairOfKyes = [String]()
+//
+//let halfKey: Int = fullKey.count / 2
+//let firstKey = fullKey[fullKey.startIndex..<fullKey.index(fullKey.startIndex, offsetBy: halfKey)]
+//let secondKey = fullKey[fullKey.index(fullKey.startIndex, offsetBy: halfKey)..<fullKey.endIndex]
+//
+//print(firstKey)
+//print(type(of: firstKey))
+//print(secondKey)
+//print(type(of: secondKey))
+//
+////pairOfKyes.append(firstKey)
+////pairOfKyes.append(secondKey)
+//
+////    return pairOfKyes
+////}
 
-let halfKey: Int = fullKey.count / 2
-let firstKey = fullKey[fullKey.startIndex..<fullKey.index(fullKey.startIndex, offsetBy: halfKey)]
-let secondKey = fullKey[fullKey.index(fullKey.startIndex, offsetBy: halfKey)..<fullKey.endIndex]
 
-print(firstKey)
-print(type(of: firstKey))
-print(secondKey)
-print(type(of: secondKey))
 
-//pairOfKyes.append(firstKey)
-//pairOfKyes.append(secondKey)
-
-//    return pairOfKyes
-//}
 
 
